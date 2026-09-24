9982c99f5d0ed730e68e2332867cce3d87a6baad
Vasylenko Dmytro <vasilenkodima292@gmail.com>
2026-09-24 23:36:30 +0300

    feat(core): implement RAM class with lazy memory allocation

---
+++ src/core/ram.cpp
@@ -0,0 +1,119 @@
+#include "ram.h"
+#include <algorithm>
+#include <cstring>
+
+namespace Core
+{
+
+Ram::Ram(
+    size_t blockCount, size_t blockSize,
+    RamUninitBehavior ramBehavior
+    ) : m_blockCount(blockCount),
+    m_blockSize(blockSize),
+    m_blocks(blockCount),
+    m_ramBehavior(ramBehavior)
+{
+    m_capacity = m_blockCount * m_blockSize;
+}
+
+BusStatus Ram::readBlock(uint64_t addr, uint8_t* buf, size_t size)
+{
+    if (addr >= m_capacity || size > m_capacity || size > (m_capacity - addr))
+        return BusStatus::UnmappedAddress;
+
+    size_t blockIndex = addr / m_blockSize;
+    size_t offset = addr % m_blockSize;
+
+    size_t bytesRead = 0;
+    while (bytesRead < size)
+    {
+        size_t bytesToRead = std::min(size - bytesRead, m_blockSize - offset);
+
+        BusStatus status = readBytesWithUninitBehavior(
+            blockIndex,
+            offset,
+            buf + bytesRead,
+            bytesToRead
+            );
+
+        if (status != BusStatus::Ok)
+            return status;
+
+        bytesRead += bytesToRead;
+        ++blockIndex;
+        offset = 0;
+    }
+
+    return BusStatus::Ok;
+}
+
+BusStatus Ram::writeBlock(uint64_t addr, const uint8_t* buf, size_t size)
+{
+    if (addr >= m_capacity || size > m_capacity || size > (m_capacity - addr))
+        return BusStatus::UnmappedAddress;
+
+    size_t blockIndex = addr / m_blockSize;
+    size_t offset = addr % m_blockSize;
+
+    size_t bytesWritten = 0;
+    while (bytesWritten < size)
+    {
+        allocateBlock(blockIndex);
+
+        uint8_t* blockBytes = m_blocks[blockIndex].get();
+        size_t bytesToWrite = std::min(size - bytesWritten, m_blockSize - offset);
+        std::memcpy(&blockBytes[offset], &buf[bytesWritten], bytesToWrite);
+
+        std::fill_n(m_initializedMask[blockIndex].get() + offset, bytesToWrite, true);
+
+        bytesWritten += bytesToWrite;
+        ++blockIndex;
+        offset = 0;
+    }
+
+    return BusStatus::Ok;
+}
+
+BusStatus Ram::readBytesWithUninitBehavior(
+    size_t blockIndex,
+    size_t offset,
+    uint8_t* destBuf,
+    size_t bytesToRead
+    )
+{
+    // if block exists, read actual data
+    if (m_blocks[blockIndex])
+    {
+        uint8_t* blockBytes = m_blocks[blockIndex].get();
+        std::memcpy(destBuf, &blockBytes[offset], bytesToRead);
+        return BusStatus::Ok;
+    }
+
+    // else apply behavior for uninitialized memory
+    switch (m_ramBehavior)
+    {
+    case RamUninitBehavior::ZeroFill:
+        std::fill_n(destBuf, bytesToRead, 0);
+        return BusStatus::Ok;
+    }
+
+    return BusStatus::AccessFault;
+}
+
+void Ram::allocateBlock(size_t blockIndex)
+{
+    if (m_blocks[blockIndex])
+        return;
+
+    m_blocks[blockIndex] = std::make_unique<uint8_t[]>(m_blockSize);
+    m_initializedMask[blockIndex] = std::make_unique<bool[]>(m_blockSize);
+
+    switch (m_ramBehavior)
+    {
+    case RamUninitBehavior::ZeroFill:
+        std::fill_n(m_blocks[blockIndex].get(), m_blockSize, 0);
+        break;
+    }
+}
+
+} // namespace Core
---
+++ src/core/ram.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "bus_device_interface.h"
+#include <cstdint>
+#include <vector>
+#include <memory>
+
+namespace Core
+{
+
+enum class RamUninitBehavior
+{
+    ZeroFill
+    // PatternFill,
+    // RandomOnPowerOn,
+    // HardwareFault
+};
+
+class Ram : public IBusDevice
+{
+private:
+    size_t m_capacity;
+
+    // size of a single physically allocated block
+    uint32_t m_blockSize;
+    uint32_t m_blockCount;
+
+    std::vector<std::unique_ptr<uint8_t[]>> m_blocks;
+    // holds true if the corresponding memory cell has been written at least once
+    std::vector<std::unique_ptr<bool[]>> m_initializedMask;
+
+    RamUninitBehavior m_ramBehavior;
+
+    void allocateBlock(size_t blockIndex);
+    BusStatus readBytesWithUninitBehavior(
+        size_t blockIndex,
+        size_t offset,
+        uint8_t* destBuf,
+        size_t bytesToRead
+        );
+public:
+    Ram(
+        size_t blockCount, size_t blockSize=4*1024,
+        RamUninitBehavior ramBehavior=RamUninitBehavior::ZeroFill
+        );
+
+    BusStatus readBlock(uint64_t addr, uint8_t* buf, size_t size) override;
+    BusStatus writeBlock(uint64_t addr, const uint8_t* buf, size_t size) override;
+};
+
+} // namespace Core
